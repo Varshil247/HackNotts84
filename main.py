3a221f2104de72ffdd98adad7d92e1a00fe98c0c
Varshil247 <varshil247@gmail.com>
2023-11-04 18:55:38 +0000

    added app

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import requests
 import pygame
 from io import BytesIO
+import tkinter as tk
 
 def getAudio():
     recognizer = sr.Recognizer()
@@ -88,3 +89,39 @@ def  makeAudio(response_text):
 
     else:
         print('Failed to retrieve the audio file.')
+
+
+
+# Create the main application window
+app = tk.Tk()
+app.title("ChatAI")
+app.geometry("300x500")
+
+#input
+inputFrame = tk.Frame(app)
+inputFrame.pack(expand=True, fill="both")
+
+inputlabel = tk.Label(inputFrame, text="Input", bg="#EAEFD3")
+inputlabel.pack(expand=True, fill="both")
+
+#output
+outputFrame = tk.Frame(app)
+outputFrame.pack(expand=True, fill="both")
+
+outputlabel = tk.Label(outputFrame, text="Output", bg="#EAEFD3")
+outputlabel.pack(expand=True, fill="both")
+
+#controls
+controlsFrame = tk.Frame(app)
+controlsFrame.pack(expand=True, fill="both")
+
+startButton = tk.Button(controlsFrame, text="Start", bg="#B3C0A4", command=getAudio)
+startButton.pack(expand=True, fill="both", side="left")
+
+# stopButton = tk.Button(controlsFrame, text="Stop", bg="#505168")# , command=stopRec)
+# stopButton.pack(expand=True, fill="both", side="right")
+
+resetButton = tk.Button(controlsFrame, text="Reset", bg="#505168")# , command=stopRec)
+resetButton.pack(expand=True, fill="both", side="right")
+
+app.mainloop() 
